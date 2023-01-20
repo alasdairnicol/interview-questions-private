b958c6384695556474d44744f9ceafe3384cebb4
Alasdair Nicol <alasdair@trustedhousesitters.com>
2023-01-20 10:56:58 +0000

    Remove debugging

--- backend/ths/listings/views.py
+++ backend/ths/listings/views.py
@@ -7,9 +7,3 @@ from .serializers import ListingSerializer
 class ListingList(generics.ListAPIView):
     serializer_class = ListingSerializer
     queryset = Listing.objects.all()
-
-    def get(self, *args, **kwargs):
-        response = super().get(*args, **kwargs)
-        from django.db import connection
-        print(len(connection.queries))
-        return response
